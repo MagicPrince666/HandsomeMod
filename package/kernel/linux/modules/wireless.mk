c74888064c5513da8eab0e941ae573da69a6f641
MagicPrince666 <846863428@qq.com>
2023-03-07 08:14:11 +0800

    add esp32 driver

--- package/kernel/linux/modules/wireless.mk
+++ package/kernel/linux/modules/wireless.mk
@@ -42,6 +42,23 @@ endef
 
 $(eval $(call KernelPackage,net-rtl8192su))
 
+define KernelPackage/net-esp32
+  $(call KernelPackage/mac80211/Default)
+  SUBMENU:=$(WIRELESS_MENU)
+  TITLE:=ESP32 WIFI support (staging)
+  DEPENDS:=+kmod-mmc +kmod-mac80211
+  KCONFIG:=\
+	CONFIG_ESP32_SDIO
+  FILES:=$(LINUX_DIR)/drivers/net/wireless/esp32/esp32_sdio.ko
+  AUTOLOAD:=$(call AutoProbe,esp32_sdio)
+endef
+
+define KernelPackage/net-esp32/description
+ Kernel modules for ESP32 SDIO WIFI fullmac support.
+endef
+
+$(eval $(call KernelPackage,net-esp32))
+
 define KernelPackage/cfg80211-old
   SUBMENU:=$(WIRELESS_MENU)
   TITLE:=using orginal kernel's cfg80211
