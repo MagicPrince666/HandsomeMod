7d2adbf7d1aa59a5b9a625c597f5e19852201b78
MagicPrince666 <846863428@qq.com>
2023-06-12 13:46:34 +0800

    update package

--- package/prince/esp32-control/src/ctrl_config.h
+++ package/prince/esp32-control/src/ctrl_config.h
@@ -54,8 +54,8 @@
 
 /* station mode */
 #define STATION_MODE_MAC_ADDRESS            "aa:bb:cc:dd:ee:ff"
-#define STATION_MODE_SSID                   "MyWifi"
-#define STATION_MODE_PWD                    "MyWifiPass@123"
+#define STATION_MODE_SSID                   "OpenWrt_TX1801_PLUS_2.4G"
+#define STATION_MODE_PWD                    "67123236"
 #define STATION_MODE_BSSID                  ""
 #define STATION_MODE_IS_WPA3_SUPPORTED      false
 #define STATION_MODE_LISTEN_INTERVAL        5
